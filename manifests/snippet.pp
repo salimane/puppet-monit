6c90d422d3119b393b91e08e7979c9c7d780c633
Salimane Adjao Moustapha <me@salimane.com>
2013-06-08 21:09:41 +0800

    make puppet-lint happy

--- manifests/snippet.pp
+++ manifests/snippet.pp
@@ -2,7 +2,8 @@
 # Creates a monit configuration snippet in /etc/monit/conf.d/
 #
 # Parameters:
-#   namevar - the name of this resource will be used for the configuration file name
+#   namevar - the name of this resource will be used for
+#           the configuration file name
 #   ensure  - present or absent
 #   content - as for the "file" type
 #   source  - as for the "file" type
@@ -21,24 +22,20 @@
 #     customlines => ["if failed port 22 then restart"]
 #   }
 # (end)
-define monit::snippet($ensure=present,$target="",$source="",$content="") {
-	file {"/etc/monit/conf.d/$name.monitrc":
-                ensure => $ensure,
-		owner   => "root",
-		group   => "root",
-		mode    => 0400,
-		notify  => Service["monit"],
-                content => $content ? {
-                        ""      => undef,
-                        default => $content
-                },
-                source => $source ? {
-                        ""      => undef,
-                        default => $source
-                },
-                target => $target ? {
-                        ""      => undef,
-                        default => $target
-                },
-        }
+define monit::snippet (
+	$ensure  = present,
+	$target  = '',
+	$source  = '',
+	$content = ''
+) {
+	file {"/etc/monit/conf.d/${name}.monitrc":
+		ensure  => $ensure,
+		owner   => 'root',
+		group   => 'root',
+		mode    => '0400',
+		notify  => Service['monit'],
+		content => $content ? {'' => undef, default => $content},
+		source  => $source ? {'' => undef, default => $source},
+		target  => $target ? {'' => undef, default => $target},
+	}
 }
