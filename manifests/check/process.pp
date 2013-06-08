9d2364155b3b35a437093a7c348c6e489209f64d
Salimane Adjao Moustapha <me@salimane.com>
2013-06-08 21:09:41 +0800

    make puppet-lint happy

--- manifests/check/process.pp
+++ manifests/check/process.pp
@@ -6,11 +6,13 @@
 #   pidfile     - the pidfile monit will check
 #   start       - the command used by monit to start the service
 #   stop        - the command used by monit to stop the service
-#   customlines - lets you inject custom lines into the monitrc snippet, just pass an array, and it will appear in the configuration file
+#   customlines - lets you inject custom lines into the monitrc snippet,
+#            just pass an array, and it will appear in the configuration file
 #
 # Actions:
 #   The following actions gets taken by this defined type:
-#    - creates /etc/monit/conf.d/namevar.monitrc as root:root mode 0400 based on _template_
+#    - creates /etc/monit/conf.d/namevar.monitrc
+#            as root:root mode 0400 based on _template_
 #
 # Requires:
 #   - Package["monit"]
@@ -26,20 +28,23 @@
 #   }
 # (end)
 
-define monit::check::process($ensure=present, $process=$name,
-                             $pidfile="/var/run/$name.pid",
-                             $start="/etc/init.d/$name start",
-                             $start_extras="",
-                             $stop="/etc/init.d/$name stop",
-                             $stop_extras="",
-                             $customlines="") {
-	file {"/etc/monit/conf.d/$name.monitrc":
+define monit::check::process(
+	$ensure       = present,
+	$process      = $name,
+	$pidfile      = "/var/run/${name}.pid",
+	$start        = "/etc/init.d/${name} start",
+	$start_extras = '',
+	$stop         = "/etc/init.d/${name} stop",
+	$stop_extras  = '',
+	$customlines  = ''
+) {
+	file {"/etc/monit/conf.d/${name}.monitrc":
 		ensure  => $ensure,
-		owner   => "root",
-		group   => "root",
-		mode    => 0400,
-		content => template("monit/check_process.monitrc.erb"),
-		notify  => Service["monit"],
+		owner   => 'root',
+		group   => 'root',
+		mode    => '0400',
+		content => template('monit/check_process.monitrc.erb'),
+		notify  => Service['monit'],
 	}
 }
 
--- manifests/init.pp
+++ manifests/init.pp
@@ -44,82 +44,87 @@
 class monit {
 
 
-	# The monit_secret is used with the fqdn of the host to make a
-	# password for the monit http server.
-	$monit_default_secret="This is not very secret, is it?"
+    # The monit_secret is used with the fqdn of the host to make a
+    # password for the monit http server.
+    $monit_default_secret='This is not very secret, is it?'
 
-	# The default alert recipient.  You can override this by setting the
-	# variable "$monit_alert" in your node specification.
-	$monit_default_alert="root@localhost"
+    # The default alert recipient.  You can override this by setting the
+    # variable "$monit_alert" in your node specification.
+    $monit_default_alert='root@localhost'
 
-        # How often should the daemon pool? Interval in seconds.
-        case $monit_pool_interval {
-          '': { $monit_pool_interval = '120' }
+    # How often should the daemon pool? Interval in seconds.
+    case $monit_pool_interval {
+        '': { $monit_pool_interval = '120' }
+        default: {}
+    }
+
+    # Should the httpd daemon be enabled, or not? By default it is not
+    case $monit_enable_httpd {
+        '': { $monit_enable_httpd = 'no' }
+        default: {}
+    }
+    # The package
+    package { 'monit':
+        ensure => installed,
+    }
+
+    # The service
+    service { 'monit':
+        ensure  => running,
+        require => Package['monit'],
+    }
+
+    # How to tell monit to reload its configuration
+    exec { 'monit reload':
+        provider    => shell,
+        command     => '`which monit` reload',
+        refreshonly => true,
+    }
+
+    # Default values for all file resources
+    File {
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0400',
+        notify  => Exec['monit reload'],
+        require => Package['monit'],
+    }
+
+    # The main configuration directory, this should have been provided by
+    # the "monit" package, but we include it just to be sure.
+    file { '/etc/monit':
+        ensure  => directory,
+        mode    => '0700',
+    }
+
+    # The configuration snippet directory.  Other packages can put
+    # *.monitrc files into this directory, and monit will include them.
+    file { '/etc/monit/conf.d':
+        ensure  => directory,
+        mode    => '0700',
+    }
+
+    # The main configuration file
+    file { '/etc/monit/monitrc':
+        content => template('monit/monitrc.erb'),
+    }
+
+    # Monit is disabled by default on debian / ubuntu
+    case $::operatingsystem {
+        'debian': {
+            file { '/etc/default/monit':
+                content => "startup=1\n
+                CHECK_INTERVALS=${monit_pool_interval}\n",
+                before  => Service['monit']
+            }
         }
-
-        # Should the httpd daemon be enabled, or not? By default it is not
-        case $monit_enable_httpd {
-          '': { $monit_enable_httpd = 'no' }
+        default: {
         }
-	# The package
-	package { "monit":
-		ensure => installed,
-	}
+    }
 
-	# The service
-	service { "monit":
-		ensure  => running,
-		require => Package["monit"],
-	}
-
-	# How to tell monit to reload its configuration
-	exec { "monit reload":
-		provider	=> shell,
-		command		=> "`which monit` reload",
-		refreshonly	=> true,
-	}
-
-	# Default values for all file resources
-	File {
-		owner   => "root",
-		group   => "root",
-		mode    => 0400,
-		notify  => Exec["monit reload"],
-		require => Package["monit"],
-	}
-
-	# The main configuration directory, this should have been provided by
-	# the "monit" package, but we include it just to be sure.
-	file { "/etc/monit":
-			ensure  => directory,
-			mode    => 0700,
-	}
-
-	# The configuration snippet directory.  Other packages can put
-	# *.monitrc files into this directory, and monit will include them.
-	file { "/etc/monit/conf.d":
-			ensure  => directory,
-			mode    => 0700,
-	}
-
-	# The main configuration file
-	file { "/etc/monit/monitrc":
-		content => template("monit/monitrc.erb"),
-	}
-
-	# Monit is disabled by default on debian / ubuntu
-	case $operatingsystem {
-		"debian": {
-			file { "/etc/default/monit":
-				content => "startup=1\nCHECK_INTERVALS=${monit_pool_interval}\n",
-				before  => Service["monit"]
-			}
-		}
-	}
-
-	# A template configuration snippet.  It would need to be included,
-	# since monit's "include" statement cannot handle an empty directory.
-	monit::snippet{ "monit_template":
-		source => "puppet://$server/modules/monit/template.monitrc",
-	}
+    # A template configuration snippet.  It would need to be included,
+    # since monit's "include" statement cannot handle an empty directory.
+    monit::snippet{ 'monit_template':
+        source => "puppet://${server}/modules/monit/template.monitrc",
+    }
 }
